61d2a1907827ff785d6e1657360e3c8ad1d63847
K.M Shariat Ullah <cast.shariat@gmail.com>
2022-08-17 12:47:59 +0600

    added xscale and yscale

--- main.py
+++ main.py
@@ -14,6 +14,20 @@ def scale(magnitude, theta, scalingFactor):
     new_tip = [x_comp, y_comp]
     return new_tip 
 
+#this xscale function only scale in the x axis
+def xscale(magnitude, theta, xscalingfactor):
+    x_comp = xscalingfactor*magnitude*np.cos(theta)
+    y_comp = magnitude*np.sin(theta)
+    new_tip = [x_comp, y_comp]
+    return new_tip
+    
+#this yscale function only scale in the y axis
+def yscale(magnitude, theta, yscalingfactor):
+    x_comp = magnitude*np.cos(theta)
+    y_comp = yscalingfactor*magnitude*np.sin(theta)
+    new_tip = [x_comp, y_comp]
+    return new_tip
+
 # resultant of two vectors when magnitude of both of them and angle between them is given
 def resultant(magnitude1, magnitude2, alpha):
     resultant = np.sqrt(magnitude1**2 + magnitude2**2 + 2*magnitude1*magnitude2*np.cos(alpha))
