e507d94e530bf8cef2498ae478af1cebad252fb4
K.M Shariat Ullah <cast.shariat@gmail.com>
2022-08-17 13:38:29 +0600

    from radians to degree conversion

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def angle(i,j,k):
 
 #component function gives us the x_component and y_component of a vector.
 def component(magnitude, theta):
+    theta = theta*(np.pi/180)
     x_comp = magnitude*np.cos(theta)
     y_comp = magnitude*np.sin(theta)
     comp = [x_comp, y_comp]
@@ -21,6 +22,7 @@ def component(magnitude, theta):
 
 #scaling the whole vector
 def scale(magnitude, theta, scalingFactor):
+    theta = theta*(np.pi/180)
     x_comp = scalingFactor*magnitude*np.cos(theta)
     y_comp = scalingFactor*magnitude*np.sin(theta)
     new_tip = [x_comp, y_comp]
@@ -28,6 +30,7 @@ def scale(magnitude, theta, scalingFactor):
 
 #this xscale function only scale in the x axis
 def xscale(magnitude, theta, xscalingfactor):
+    theta = theta*(np.pi/180)
     x_comp = xscalingfactor*magnitude*np.cos(theta)
     y_comp = magnitude*np.sin(theta)
     new_tip = [x_comp, y_comp]
@@ -35,6 +38,7 @@ def xscale(magnitude, theta, xscalingfactor):
     
 #this yscale function only scale in the y axis
 def yscale(magnitude, theta, yscalingfactor):
+    theta = theta*(np.pi/180)
     x_comp = magnitude*np.cos(theta)
     y_comp = yscalingfactor*magnitude*np.sin(theta)
     new_tip = [x_comp, y_comp]
@@ -42,5 +46,6 @@ def yscale(magnitude, theta, yscalingfactor):
 
 # resultant of two vectors when magnitude of both of them and angle between them is given
 def resultant(magnitude1, magnitude2, alpha):
+    alpha = alpha*(np.pi/180)
     resultant = np.sqrt(magnitude1**2 + magnitude2**2 + 2*magnitude1*magnitude2*np.cos(alpha))
     return resultant
